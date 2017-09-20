0eb24bd72dff331a870f207e235d82ca27774ff9
Komaldeep Chahal <komaldeep1993@gmail.com>
2017-09-20 12:14:41 +0200

    router with get request

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ var express = require('express');
 
 var app = express();
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 8000;
+
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+  .get(function (req,res) {
+    var responseJson = {hello: 'this is my api'};
+    res.json(responseJson);
+  });
+
+app.use('/api', bookRouter)
 
 app.get('/', function (req, res) {
   res.send(('Welcome to my API'));
