0921fdc17a53b14a393c34ee173ff020dbb2d3c3
Komaldeep Chahal <komaldeep1993@gmail.com>
2017-09-29 16:38:52 +0200

    pushing post features with better architecture of project

--- app.js
+++ app.js
@@ -14,42 +14,9 @@ var bookRouter = express.Router();
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-bookRouter.route('/Books')
-  .post(function (req, res) {
-    var book = new Book(req.body);
+bookRouter = require('./Routes/bookRoutes');
 
-    console.log(book)
-    res.send(book)
-  })
-  .get(function (req,res) {
-
-    // var query = { }
-    // if(req.query.genre){
-    //   query.genre = req.query.genre;
-    // }
-    //
-    // Book.find(query, function (err, books) {
-    //   if(err){
-    //     // res.status(500).send(err)
-    //     console.log(err)
-    //   }
-    //   else {
-    //     res.json(books)
-    //   }
-    // })
-
-    Book.find(function (err, books) {
-      if(err){
-        // res.status(500).send(err)
-        console.log(err)
-      }
-      else {
-        res.json(books)
-      }
-    })
-  });
-
-app.use('/api', bookRouter)
+app.use('/api/books', bookRouter)
 
 app.get('/', function (req, res) {
   res.send(('Welcome to my API'));
