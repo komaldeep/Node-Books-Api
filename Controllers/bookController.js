e7ac00ee97a2c77898322f937087dec50d11490c
Komaldeep Chahal <komaldeep1993@gmail.com>
2017-10-04 16:03:37 +0200

    adding unit test

--- Controllers/bookController.js
+++ Controllers/bookController.js
@@ -1,8 +1,16 @@
 var bookControllers = function (Book) {
   var post = function (req, res) {
     var book = new Book(req.body);
-    book.save();
-    res.status(201).send(book)
+
+    if(!req.body.title){
+      res.status(400);
+      res.send('Title is required')
+    }
+    else {
+      book.save();
+      res.status(201);
+      res.send(book);
+    }
   }
 
   var get = function (req, res) {
--- Test/bookControllerTests.js
+++ Test/bookControllerTests.js
@@ -3,6 +3,24 @@ var should = require('should'),
 
 describe('Book controller Tests:', function () {
   describe('Post', function () {
-    
+    it('should not allow an empty title on post', function () {
+      var Book = function (book){ this.save = function () { }};
+      var req = {
+        body:{
+          author: 'king of the world'
+        }
+      };
+      var res = {
+        status: sinon.spy(),
+        send: sinon.spy()
+      };
+
+      var bookController = require('../Controllers/bookController')(Book);
+
+      bookController.post(req, res);
+
+      res.status.calledWith(400).should.equal(true, 'Bad status ' + res.status);
+      res.send.calledWith('Title is required').should.equal(true);
+    })
   })
 })
